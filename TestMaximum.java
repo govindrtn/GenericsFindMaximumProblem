2e1879c68f11b3442d37c62c4c646f0b19030897
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-20 22:28:45 +0530

    Added : Three Float number find Maximum

--- TestMaximum.java
+++ TestMaximum.java
@@ -4,26 +4,25 @@ public class TestMaximum {
 	
 	public static void main(String[] args) {
 		
-		Integer num1 = 4; 
-		Integer num2 = 5; 
-		Integer num3 = 9;
-		Integer result = TextMaximum(num1,num2,num3);
-		
+		Float num1 = 4.4F; 
+		Float num2 = 5.5F; 
+		Float num3 = 9.9F;
+		Float result = TextMaximum(num1,num2,num3);
 		System.out.println(result);
 		
 	}
-	public static Integer TextMaximum(Integer number1, Integer number2 , Integer number3) {
+	public static Float TextMaximum(Float number1, Float number2 , Float number3) 
+	{
+		Float maxNumber = number1;
 		
-		Integer maxNumber = number1;
-		
-		if (number2.compareTo(maxNumber)>0) {
-			
+		if (number2.compareTo(maxNumber)>0)
+		{	
 			 maxNumber = number2;
 		}
-		if (number3.compareTo(maxNumber)>0) {
+		if (number3.compareTo(maxNumber)>0)
+		{
 			maxNumber = number3;
 		}
 		return maxNumber;
 	}
-
 }
